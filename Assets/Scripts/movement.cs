3c4d4f1d79d54bea063dab5f090c6c93fa8186b8
SwiftSwrd <savage_sw450@hotmail.com>
2014-09-28 09:49:40 -0600

    Debugging attempts at selection

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -3,11 +3,19 @@ using System.Collections;
 
 public class movement : MonoBehaviour {
 
+	public bool selected;
+	public GameObject current;
+
+
 	public float moveSpeed;
 	private Vector2 move;
 
 	// Use this for initialization
 	void Start () {
+		if (gameObject.CompareTag ("player")) {
+			selected = true;
+		}
+
 		moveSpeed = 5.0f;
 		move = new Vector2 (0.0f, 0.0f);
 	}
@@ -20,33 +28,49 @@ public class movement : MonoBehaviour {
 		Debug.Log ("Collision detected");
 	}
 
+	void OnMouseUp() {
+		//selected = false;
+		Debug.Log ("1");
+	}
+
+	void OnMouseUpAsButton() {
+		//selected = true;
+		Debug.Log ("2");
+	}
+
+	void OnMouseDown() {
+		//selected = true;
+		Debug.Log ("3");
+	}
+
+
+
 	void FixedUpdate () {
+		if(selected){
+			move.x = 0.0f;
+			move.y = 0.0f;
 
-		move.x = 0.0f;
-		move.y = 0.0f;
+			if(Input.GetKey(KeyCode.D)) {
+				move.x += moveSpeed;
+			}
+			
+			if(Input.GetKey(KeyCode.A)) {
+				move.x += -moveSpeed;
+			}
+			
+			if(Input.GetKey(KeyCode.W)) {
+				move.y += moveSpeed;
+			}
+			
+			if(Input.GetKey(KeyCode.S)) {
+				move.y += -moveSpeed;
+			}
 
-		if(Input.GetKey(KeyCode.D)) {
-			move.x += moveSpeed;
+			rigidbody2D.velocity = move;
 		}
-		
-		if(Input.GetKey(KeyCode.A)) {
-			move.x += -moveSpeed;
-		}
-		
-		if(Input.GetKey(KeyCode.W)) {
-			move.y += moveSpeed;
-		}
-		
-		if(Input.GetKey(KeyCode.S)) {
-			move.y += -moveSpeed;
-		}
-
-		rigidbody2D.velocity = move;
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
 	}
 }

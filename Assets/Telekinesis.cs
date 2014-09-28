2c0369a7c0eddce9b9a64a71bd9b4b531f5f6667
Kraysus <syuveil316@yahoo.ca>
2014-09-27 22:10:07 -0400

    Mazes
    All to mazes be realized

---
+++ Assets/Telekinesis.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Telekinesis : MonoBehaviour {
+
+	public bool Mpressed;
+
+	// Use this for initialization
+	void Start () {
+		Mpressed = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//if (Input.GetMouseButton) 
+		{
+			
+		}
+	}
+	void OnMouseDown() {
+
+	}
+}
